6567400e9db676bbc9ca036c8bb99396d3e3b32b
kunz <bboykunz@gmail.com>
2024-05-20 08:37:32 +0800

    Add icon.js for close icon

---
+++ icon.js
@@ -0,0 +1,5 @@
+export const closeIconHtml = `<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.75 3.16L14.34 1.75L8.75 7.34L3.16 1.75L1.75 3.16L7.34 8.75L1.75 14.34L3.16 15.75L8.75 10.16L14.34 15.75L15.75 14.34L10.16 8.75L15.75 3.16Z" fill="#313131"/>
+                            </svg>`;
+
+
