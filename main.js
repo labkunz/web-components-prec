ddd53c8a9404df9acea725859a5af07a2382e90c
KentChen <bboykunz@gmail.com>
2024-05-20 11:25:29 +0800

    Change to module type to use

--- main.js
+++ main.js
@@ -1,43 +1,5 @@
-// 先繼承HTMLElement來擴充
-
-class PopupInfo extends HTMLElement {
-    constructor () {
-        super();
-    }
-
-    connectedCallback () {
-        // 建立 shadow root
-        const shadow = this.attachShadow({ mode: "open" });  //js可以造訪
-        //設定無法造訪: closed
-
-        // 建立 所需元素
-        const card = document.createElement("div");
-        card.setAttribute("class", "panel");
-
-        const cardHeader = document.createElement("div");
-        cardHeader.setAttribute("class", "header");
-
-        const text = document.createElement("p");
-        text.setAttribute("class", "title");
-
-        // 取得attribute content放入text裡面
-        const textContent = this.getAttribute("data-text");
-        text.textContent = textContent;
-
-        // 組裝元素
-        cardHeader.appendChild(text);
-        card.appendChild(cardHeader);
-
-        // 替換使用外部樣式
-        const linkEle = document.createElement("link");
-        linkEle.setAttribute("rel", "stylesheet");
-        linkEle.setAttribute("href", "./style.css")
-
-        // 把相關元素加入至 shadow DOM
-        shadow.appendChild(linkEle);
-        shadow.appendChild(card);
-    }
-}
+import PopupInfo from "./modal.js";
 
 // 定義新元素
+
 customElements.define("custom-modal", PopupInfo);
